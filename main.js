5462a5e45a9a1f5ad0ff97bd47ac0decb8bba9db
Piero Montalvo <piero.montalvo9922@gmail.com>
2020-06-06 09:30:52 -0500

    Primer Retos

---
+++ main.js
@@ -0,0 +1,62 @@
+// // EJERCICIO Nº1
+
+// function suma(x,y){
+//     let resultado = x + y
+
+//     return `La suma de ${x} + ${y} es = ${resultado}`
+// }   
+
+// console.log(suma(10,10))
+
+
+// // EJERCICIO Nº2
+
+// function potencia(n,p){
+//     let resultado = n**p
+//     return resultado
+// }
+
+// console.log(potencia(2,2))
+
+
+// // EJERCICIO Nº3
+
+// function sumOfCubes(num1,num2,num3){
+//     let num1Cubo = num1 **3
+//     let num2Cubo = num2 **3
+//     let num3Cubo = num3 **3
+
+//     let resultadoCubo = num1Cubo + num2Cubo + num3Cubo
+
+//     return resultadoCubo
+// }
+
+// console.log(sumOfCubes(3,2,5))
+
+
+// // EJERCICIO Nº4
+
+// function triArea(base,altura){
+//     let resultado = (base * altura) / 2
+//     return `El Area del Triangulo es : ${resultado}`
+// }
+
+// console.log(triArea(2,2))
+
+
+// // EJERCICIO Nº5
+
+
+function calculator(n1,n2,x){
+    x = Number(n1 * n2)
+
+    if((typeof n1 === Number) && (typeof n2 === Number)){
+        return `El resultado de la Multiplicacion de ${n1} * ${n2} es : ${x}`
+    }else{
+        return 'El parametro no es reconocido'
+    }
+}
+
+console.log(calculator(40,10))
+
+
